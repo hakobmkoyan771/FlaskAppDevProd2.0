e7d8ee6d2e11f8b574bf662de8ce567e1d4b6946
Hakob Mkoyan <hakobmkoyan771@gmail.com>
2022-04-13 23:28:46 +0400

    .

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import argparse
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,5 +9,8 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.debug = True
+    parser = argparse.ArgumentParser(description='Pass debug variable')
+    parser.add_argument('--deb', required=True, type=bool, help='booleand debug argument')
+    args = parser.parse_args()
+    app.debug = args.deb
     app.run(host="0.0.0.0", port="5050")    
