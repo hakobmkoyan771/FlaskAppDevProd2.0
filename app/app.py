6a9592498b2e7f3e2f5953c0bb51cd63d4b792ea
hakobmkoyan771 <hakobmkoyan771@gmail.com>
2022-04-23 19:39:57 +0400

    .

--- app/app.py
+++ app/app.py
@@ -5,13 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-<<<<<<< HEAD
 def hello():
     return "Hello!"
-=======
-def main():
-    return "Main Page"
->>>>>>> 7e37c2b (.)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pass debug variable')
