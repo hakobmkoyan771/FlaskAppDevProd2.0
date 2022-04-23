c359658aa816bcf8c132674aa1c6d737bbaef7b1
hakobmkoyan771 <hakobmkoyan771@gmail.com>
2022-04-23 18:47:17 +0400

    .

--- app/app.py
+++ app/app.py
@@ -5,12 +5,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+<<<<<<< HEAD
 def hello():
     return "Hello!"
+=======
+def main():
+    return "Main Page"
+>>>>>>> 7e37c2b (.)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pass debug variable')
-    parser.add_argument('--deb', required=True, type=bool, help='booleand debug argument')
+    parser.add_argument('--deb', required=True, type=bool, help='boolean debug argument')
     args = parser.parse_args()
     app.debug = args.deb
     app.run(host="0.0.0.0", port="5050")    
